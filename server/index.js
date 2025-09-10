903cf276e645fae8e95aaab97651da5ce527b7b1
Santiago Lezica <salezica@gmail.com>
2025-09-10 16:03:47 -0300

    Bootstrap server and web

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+})
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
