0318e8fb5ee6694392f6c2fe44c98a457a72ac8c
Santiago Lezica <salezica@gmail.com>
2025-09-11 21:32:18 -0300

    client: remove header

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -5,9 +5,6 @@ import './App.css'
 export default function App() {
   return (
     <div className="app">
-      <div className="header">
-        <h1>Timeline Garden</h1>
-      </div>
       <div className="timeline-wrapper">
         <Timeline />
       </div>
