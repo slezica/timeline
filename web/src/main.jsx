0bb005bca85e3f3997ae60dd0b9a743289ad4142
Santiago Lezica <salezica@gmail.com>
2025-09-17 19:35:53 -0300

    Adapt main

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import '@picocss/pico'
 import App from './components/App'
+import { db, initializeDb } from './database'
+
 
 const container = document.body
-if (!container) {
-  throw new Error('Root element not found')
-}
-
 const root = createRoot(container)
-
 root.render(<App />)
+
