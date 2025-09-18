39fc13893b44d23e41ec306f80b85163da4f6bbc
Santiago Lezica <salezica@gmail.com>
2025-09-18 16:19:17 -0300

    Add forgotten utils file :D

---
+++ web/src/utils.js
@@ -0,0 +1,77 @@
+import { useEffect } from "react"
+
+export function throttle(fn, ms) {
+  let pending = false
+  let running = false
+
+  function throttledFn() {
+    if (running) {
+      pending = true
+      return
+    }
+
+    running = true
+    pending = false
+
+    try {
+      fn()
+    } catch (err) {
+      console.error(err)
+    } finally {
+      running = false
+    }
+
+    if (pending) {
+      new Promise(r => setTimeout(r, ms)).then(throttledFn)
+    }
+  }
+
+  return throttledFn
+}
+
+
+export function accumulate(ms, fn) {
+  let pending = []
+  let timeout = null
+
+  return function(arg) {
+    pending.push(arg)
+    if (timeout) {
+      return
+    }
+
+    timeout = setTimeout(() => {
+      const toRun = pending
+      pending = []
+      fn(toRun)
+      timeout = null
+    }, ms)
+  }
+}
+
+
+export function scheduled(asyncFn) {
+  let pending = false
+  let running = false
+
+  return async function scheduledFn() {
+    if (running) {
+      pending = true
+      return
+    }
+
+    running = true
+    pending = false
+
+    try {
+      await asyncFn()
+
+    } catch (err) {
+      console.error(err)
+
+    } finally {
+      running = false
+      if (pending) { Promise.resolve().then(scheduledFn) }
+    }
+  }
+}
