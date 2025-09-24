c9fca48d9d93b05f70bd3f4916eff2a669ca0892
Santiago Lezica <salezica@gmail.com>
2025-09-24 01:28:52 -0300

    Use EditableList in Shelf

--- web/src/components/Shelf.jsx
+++ web/src/components/Shelf.jsx
@@ -4,51 +4,12 @@ import SmallItem from './SmallItem'
 import DropTarget from './DropTarget'
 import PlaceholderItem from "./PlaceholderItem";
 import { getTransferData } from '../utils';
+import EditableList from './EditableList';
 
 
 export default function Shelf({ onClick }) {
   const shelf = useStore(state => state.shelf)
   const items = useStore(state => state.items)
-
-  const getValidTransferData = (ev) => {
-    const data = getTransferData(ev)
-    return (data?.id && items.byId[data.id]) ? data : null
-  }
-
-  const handleSelfDrop = (ev) => {
-    const itemRef = getValidTransferData(ev)
-    if (!itemRef) { return }
-
-    const newShelfOrder = [...shelf.refs]
-
-    const prevIndex = newShelfOrder.findIndex(it => it.id == itemRef.id)
-    if (prevIndex != -1) {
-      newShelfOrder.splice(prevIndex, 1)
-    }
-    newShelfOrder.push(itemRef)
-    shelf.replace(newShelfOrder)
-  }
-
-  const handleEntryDrop = (ev) => {
-    const ref = getValidTransferData(ev)
-    if (!ref) { return }
-
-    ev.stopPropagation()
-
-    const item = items.byId[ref.id]
-    const newShelfOrder = [...shelf.refs]
-
-    const entry = findParentEntry(ev.target)
-    const newIndex = [...entry.parentElement.children].indexOf(entry)
-    const oldIndex = shelf.refs.findIndex(it => it.id == ref.id)
-
-    newShelfOrder.splice(newIndex, 0, ref)
-
-    if (oldIndex != -1) {
-      newShelfOrder.splice(oldIndex < newIndex ? oldIndex : oldIndex + 1, 1)
-    }
-    shelf.replace(newShelfOrder)
-  }
 
   const handleItemClick = (item) => {
     onClick?.(item)
@@ -70,44 +31,43 @@ export default function Shelf({ onClick }) {
     shelf.replace(newRefs)
   }
 
+  const handleListChange = (newRefs) => {
+    shelf.replace(newRefs)
+  }
+
   return <ShelfView
     refs={shelf.refs}
-    items={items.byId}
-    onSelfDrop={handleSelfDrop}
-    onEntryDrop={handleEntryDrop}
+    items={items}
+    onListChange={handleListChange}
     onItemDiscard={handleItemDiscard}
     onItemClick={handleItemClick}
   />
 }
 
 
-function ShelfView({ refs, items, onSelfDrop, onEntryDrop, onItemDiscard, onItemClick }) {
+function ShelfView({ refs, items, onListChange, onItemDiscard, onItemClick }) {
+  const eventToRef = (ev) => {
+    const data = getTransferData(ev)
+    return (data?.id && items.byId[data.id]) ? data : null
+  }
+
+  const refToChild = (ref) => (
+    items.byId[ref.id] != null
+      ? <SmallItem item={items.byId[ref.id]} onClick={onItemClick} onDiscard={onItemDiscard} />
+      : <div>Placeholder</div>
+  )
+
   return (
-    <DropTarget onDrop={onSelfDrop}>
-      <section className="shelf">
-
-        {refs.map((ref, position) =>
-          <DropTarget key={ref.id} onDrop={onEntryDrop}>
-            <div className="entry">
-              {items[ref.id] != null
-                ? <SmallItem item={items[ref.id]} onClick={onItemClick} onDiscard={onItemDiscard} />
-                : <div>mierda</div>
-              }
-            </div>
-          </DropTarget>
-
-        )}
-      </section>
-    </DropTarget>
+    <section className="shelf">
+      <EditableList
+        className="refs"
+        entries={refs}
+        isEqual={(a, b) => a.id == b.id}
+        fromEvent={eventToRef}
+        toChild={refToChild}
+        onChange={onListChange}
+      />
+    </section>
   )
 }
 
-
-function findParentEntry(el) {
-  while (el) {
-    if (el.classList.contains('entry')) { return el }
-    el = el.parentElement
-  }
-
-  return null
-}
