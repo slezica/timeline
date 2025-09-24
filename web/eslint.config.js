7bbe4f0e4d29e1a44da43701707bc90c9a2d8f92
Santiago Lezica <salezica@gmail.com>
2025-09-24 15:08:05 -0300

    Configure ESLint better

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,20 +1,32 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.js'],
+    files: ['**/*.js', '**/*.jsx'],
+    plugins: { react },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
       },
     },
+
     rules: {
       ...js.configs.recommended.rules,
+      'indent': ['error', 2, {
+        offsetTernaryExpressions: true,
+      }],
+
+      'react/jsx-uses-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
     },
   },
 ]
