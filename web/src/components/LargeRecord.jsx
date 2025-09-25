beebae2a3b1dbd6d8c5cd56b2477896f1d48c38a
Santiago Lezica <salezica@gmail.com>
2025-09-25 13:19:37 -0300

    Reimplement drop in LargeItem, best impl so far

--- web/src/components/LargeRecord.jsx
+++ web/src/components/LargeRecord.jsx
@@ -1,6 +1,6 @@
 import DropTarget from './DropTarget'
 import { useStore } from '../store'
-import RefRecord from './RefRecord'
+import TinyRecord from './TinyRecord'
 import Tag from './Tag'
 import { getTransferData, RefType, setTransferData } from '../utils'
 
@@ -22,15 +22,20 @@ export default function LargeRecord({ entries, record, onClick, onRefClick }) {
     setTransferData(ev, { id: record._id }, RefType)
   }
 
-  const handleDrop = (ev) => {
-    const data = getTransferData(ev, RefType)
-    if (!data) { return }
+  const handleRefDrop = (ev) => {
+    const ref = getTransferData(ev, RefType)
+    if (!ref || ref.id == record.id) { return }
+    ev.stopPropagation()
 
-    const ref = { id: data.id }
-    if (record._id == ref.id) { return }
-    if (record.refs.find(it => it.id == ref.id )) { return }
+    const index = [...ev.currentTarget.parentElement.children].indexOf(ev.currentTarget)
 
-    saveRecord.run({ ...record, refs: [...record.refs, ref] })
+    const newRefs = [
+      ...record.refs.slice(0, index).filter(it => it.id != ref.id),
+      ref,
+      ...record.refs.slice(index).filter(it => it.id != ref.id),
+    ]
+
+    saveRecord.run({ ...record, refs: newRefs })
   }
 
   return <LargeRecordView
@@ -39,74 +44,78 @@ export default function LargeRecord({ entries, record, onClick, onRefClick }) {
     records={records}
     onClick={handleClick}
     onRefClick={handleRefClick}
-    onDrop={handleDrop}
+    onRefDrop={handleRefDrop}
     onDragStart={handleDragStart}
   />
 }
 
 
-function LargeRecordView({ entries, record, records, onClick, onRefClick, onDrop, onDragStart }) {
+function LargeRecordView({ entries, record, records, onClick, onRefClick, onRefDrop, onDragStart }) {
   return (
-    <DropTarget onDrop={onDrop}>
-      <article
-        className={"record large " + record.kind}
-        data-id={record.id}
-        style={{ cursor: 'pointer' }}
-        draggable={true}
-        onDragStart={onDragStart}
-        onClick={onClick}
-      >
-        <header>
-          <i className={`circle ${record.kind} dot`} />
-          <strong className="title">{record.title || 'Untitled'}</strong>
+    <article
+      className   = {"record large " + record.kind}
+      draggable   = {true}
+      onDragStart = {onDragStart}
+      onClick     = {onClick}
+      data-id     = {record.id}
+    >
+      <header>
+        <i className={`circle ${record.kind} dot`} />
+        <strong className="title">{record.title || 'Untitled'}</strong>
 
-          {entries.map(entry =>
-            <span className="tags" key={entry.event}>
-              <kbd className={"tag " + entry.event}>{entry.event}</kbd>
-            </span>
-          )}
-        </header>
+        {entries.map(entry =>
+          <span className="tags" key={entry.event}>
+            <kbd className={"tag " + entry.event}>{entry.event}</kbd>
+          </span>
+        )}
+      </header>
 
-        {record.body && (
-          <div className="body">
-            {record.body}
-          </div>
+      {record.body && (
+        <div className="body">
+          {record.body}
+        </div>
+      )}
+
+      <div className="extras">
+        {record.kind == 'contact' &&
+      <div className="picture">
+        <img src={record.picture} />
+      </div>
+        }
+
+        {record.kind == 'task' &&
+        <div className="tags">
+          <Tag icon="calendar" name="Due">{formatDisplayDate(record.dueDate) || '–'}</Tag>
+          <Tag icon="calendar" name="Done">{formatDisplayDate(record.doneDate) || '–'}</Tag>
+        </div>
+        }
+
+        {record.kind == 'contact' &&
+        <div className="tags">
+          <Tag icon="envelope">{record.email || '–'}</Tag>
+
+          {record.phones.length > 0
+            ? record.phones.map((it, i) => <Tag key={i} icon="phone">{it.number}</Tag>)
+            : <Tag icon="phone"><em>Unknown</em></Tag>
+          }
+        </div>}
+      </div>
+
+      <ol className="refs">
+        {record.refs.map(ref =>
+          <DropTarget key={ref.id} onDrop={onRefDrop}>
+            <li className="ref">
+              <TinyRecord record={records.byId[ref.id]} onClick={ev => onRefClick(records.byId[ref.id])} />
+            </li>
+          </DropTarget>
         )}
 
-        <div className="extras">
-          {record.kind == 'contact' &&
-              <div className="picture">
-                <img src={record.picture} />
-              </div>
-          }
+        <DropTarget onDrop={onRefDrop}><div className="sentinel" /></DropTarget>
+      </ol>
 
-          {record.kind == 'task' &&
-              <div className="tags">
-                <Tag icon="calendar" name="Due">{formatDisplayDate(record.dueDate) || '–'}</Tag>
-                <Tag icon="calendar" name="Done">{formatDisplayDate(record.doneDate) || '–'}</Tag>
-              </div>
-          }
 
-          {record.kind == 'contact' &&
-              <div className="tags">
-                <Tag icon="envelope">{record.email || '–'}</Tag>
-
-                {record.phones.length > 0
-                  ? record.phones.map((it, i) => <Tag key={i} icon="phone">{it.number}</Tag>)
-                  : <Tag icon="phone"><em>Unknown</em></Tag>
-                }
-              </div>}
-        </div>
-
-        <div className="refs">
-          {record.refs.map(ref =>
-            <RefRecord key={ref.id} record={records.byId[ref.id]} onClick={ev => onItemClick(records.byId[ref.id])} />
-          )}
-        </div>
-
-        {window.DEBUG && <pre>{JSON.stringify(record, null, 2)}</pre>}
-      </article>
-    </DropTarget>
+      {window.DEBUG && <pre>{JSON.stringify(record, null, 2)}</pre>}
+    </article>
   )
 }
 
