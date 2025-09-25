9954fa1f41f366c7aaa91602e7092d3bd41c7ea6
Santiago Lezica <salezica@gmail.com>
2025-09-25 15:53:03 -0300

    Remove unused utils

--- web/src/utils.js
+++ web/src/utils.js
@@ -1,54 +1,5 @@
 import { useLayoutEffect } from "react"
 
-export function throttle(fn, ms) {
-  let pending = false
-  let running = false
-
-  function throttledFn() {
-    if (running) {
-      pending = true
-      return
-    }
-
-    running = true
-    pending = false
-
-    try {
-      fn()
-    } catch (err) {
-      console.error(err)
-    } finally {
-      running = false
-    }
-
-    if (pending) {
-      new Promise(r => setTimeout(r, ms)).then(throttledFn)
-    }
-  }
-
-  return throttledFn
-}
-
-
-export function accumulate(ms, fn) {
-  let pending = []
-  let timeout = null
-
-  return function(arg) {
-    pending.push(arg)
-    if (timeout) {
-      return
-    }
-
-    timeout = setTimeout(() => {
-      const toRun = pending
-      pending = []
-      fn(toRun)
-      timeout = null
-    }, ms)
-  }
-}
-
 
 export function scheduled(asyncFn) {
   let pending = false
